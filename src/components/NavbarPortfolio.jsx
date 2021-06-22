4459799cc5764a53cadb367bb8ef99993e690723
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-21 23:42:55 -0500

    Correccion en fuente de navbar

--- src/components/NavbarPortfolio.jsx
+++ src/components/NavbarPortfolio.jsx
@@ -5,7 +5,7 @@ import "../styles/NavbarPortfolio.css";
 
 export function NavbarPortfolio() {
   return (
-    <Navbar bg="dark" expand="lg" variant="dark">
+    <Navbar bg="dark" expand="lg" variant="dark" sticky="top">
       <Navbar.Brand href="#home">
         <img src={logo} className="d-inline-block align-top" alt="Logo" />
       </Navbar.Brand>
