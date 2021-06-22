a7071808ea862353910610a4566d25095e0656c7
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-22 09:54:58 -0500

    Creacion de seccion de contacto

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Contact.css";
+
+export function Contact() {
+  return (
+    <section id="Contact">
+      <h2>Contáctame</h2>
+    </section>
+  );
+}
