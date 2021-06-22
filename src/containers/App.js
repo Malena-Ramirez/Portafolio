740b9e6bd7f96e59dfc94198e36e0facb1b05696
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-21 20:00:47 -0500

    Creacion inicial de navbar

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import { NavbarPortfolio } from "../components/navbar.jsx"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarPortfolio />
+    </div>
+  );
+}
+
+export default App;
