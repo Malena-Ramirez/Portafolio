9bf877fcea16b4ada60a1406fcdbe440750d4b5e
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-23 00:33:53 -0500

    Finalizacion maquetado de habilidades

---
+++ src/components/Skill.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+
+export const Skill = ({ icon, name }) => {
+  return (
+    <Col sm className="skillset-icon d-flex flex-column align-items-center justify-content-center">
+      <i className={icon}></i>
+      <p>{name}</p>
+    </Col>
+  );
+};
--- src/components/Skillset.jsx
+++ src/components/Skillset.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Skill } from "./Skill.jsx";
+import { skillset } from "../utils/data/skillset";
+import { Container, Row } from "react-bootstrap";
 import "../styles/Skillset.css";
 
 export const Skillset = () => {
   return (
-    <section id="skillset">
+    <section id="skillset" className="d-flex flex-column align-items-center">
       <h2>Habilidades</h2>
-      <div className="skillset-icons"></div>
+      <Container className="h-100 d-flex align-items-center">
+        <Row className="skillset-icons-container">
+          {skillset.map((skill) => (
+            <Skill icon={skill.icon} name={skill.name} key={skill.name} />
+          ))}
+        </Row>
+      </Container>
     </section>
   );
 };
