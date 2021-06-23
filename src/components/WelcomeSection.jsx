dd3eb82d41419996e2949eafefed778dd435e69c
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-23 18:16:21 -0500

    Media query de welcome section

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -8,12 +8,12 @@ export function WelcomeSection() {
     <section id="welcome-section" className="d-flex align-items-center">
       <Container>
         <Row>
-          <Col s={12} md={4}>
+          <Col s={12} md={4} className="px-5">
             <img className="profile-img img-fluid" src={profile} alt="Imagen de perfil" />
           </Col>
-          <Col className="d-flex flex-column justify-content-center">
-            <h1>Soy Malena Ramírez Uribe</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, sunt recusandae quia consequatur aspernatur magnam animi expedita tempore rem nesciunt!</p>
+          <Col className="d-flex flex-column justify-content-center px-5 mt-4 welcome-text">
+            <h1>¡Hola! Soy Malena Ramírez Uribe</h1>
+            <p>Soy desarrolladora front-end en continuo proceso de formación. Amo retarme a cada día aprender cosas nuevas. Estoy siempre dispuesta a dar lo mejor de mí. Me gusta investigar y saber el funcionamiento de todo.</p>
           </Col>
         </Row>
       </Container>
