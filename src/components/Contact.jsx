1858b9287983d3c4173eaa460b8c65f045d0833e
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-23 14:05:33 -0500

    Correcciones mínimas de diseño

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ export const Contact = () => {
         <Row>
           <Col sm className="mr-5 px-5">
             <h2 className="mb-3">Contáctame</h2>
-            <p>Si te interesa trabajar conmigo en tu próximo proyecto me puedes contactar enviándome un mensaje o a través de mis redes.</p>
+            <p className="my-5">Si te interesa trabajar conmigo en tu próximo proyecto me puedes contactar enviándome un mensaje o a través de mis redes.</p>
             <div className="contact-icon d-flex justify-content-center my-5">
               <a href="https://www.linkedin.com/in/malena-ram%C3%ADrez-uribe-033643215/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin-in"> </i>
