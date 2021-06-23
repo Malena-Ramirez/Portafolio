623d7d02df5d1cca8fa127953cbb08e539d9aff7
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-23 00:03:41 -0500

    Arreglo de habilidades

---
+++ src/utils/data/skillset.js
@@ -0,0 +1,26 @@
+export const skillset = [
+  {
+    name: "HTML",
+    icon: "fab fa-html5"
+  },
+  {
+    name: "CSS",
+    icon: "fab fa-css3-alt"
+  },
+  {
+    name: "JavaScript",
+    icon: "fab fa-js-square"
+  },
+  {
+    name: "Bootstrap",
+    icon: "fab fa-bootstrap"
+  },
+  {
+    name: "Git",
+    icon: "fab fa-git-alt"
+  },
+  {
+    name: "ReactJS",
+    icon: "fab fa-react"
+  }
+];
