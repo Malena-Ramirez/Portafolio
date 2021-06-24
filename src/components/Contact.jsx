3a1335002897fb3c3871517b65e44ef5a38bbbe7
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-23 20:17:28 -0500

    Media query contacto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,10 +8,10 @@ export const Contact = () => {
     <section id="contact">
       <Container className="py-5">
         <Row>
-          <Col sm className="mr-5 px-5">
+          <Col sm className="px-5">
             <h2 className="mb-3">Contáctame</h2>
-            <p className="my-5">Si te interesa trabajar conmigo en tu próximo proyecto me puedes contactar enviándome un mensaje o a través de mis redes.</p>
-            <div className="contact-icon d-flex justify-content-center my-5">
+            <p className="my-4">Si te interesa trabajar conmigo en tu próximo proyecto me puedes contactar enviándome un mensaje o a través de mis redes.</p>
+            <div className="contact-icon d-flex justify-content-center my-3">
               <a href="https://www.linkedin.com/in/malena-ram%C3%ADrez-uribe-033643215/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin-in"> </i>
               </a>
@@ -23,7 +23,7 @@ export const Contact = () => {
               </a>
             </div>
           </Col>
-          <Col sm className="ml-5 px-5">
+          <Col sm className="px-4">
             <form action="https://formspree.io/f/xbjqynaq" method="post" className="p-3" id="contact-form">
               <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email</Form.Label>
