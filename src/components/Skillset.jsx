a39e480d2dda8210fdfc2384536c2af16bbd116a
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-23 19:12:36 -0500

    Media query de las skills

--- src/components/Skillset.jsx
+++ src/components/Skillset.jsx
@@ -8,7 +8,7 @@ export const Skillset = () => {
   return (
     <section id="skillset" className="d-flex flex-column align-items-center justify-content-center">
       <h2>Habilidades</h2>
-      <Container className="d-flex align-items-center">
+      <Container className="d-flex align-items-center justify-content-center">
         <Row className="skillset-icons-container">
           {skillset.map((skill) => (
             <Skill icon={skill.icon} name={skill.name} key={skill.name} />
