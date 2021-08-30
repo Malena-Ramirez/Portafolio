183e6aa4ad8fcec975b0bd49974725bd9a7f504a
Malena Ramirez <maramirezuribe@gmail.com>
2021-08-30 12:54:10 -0500

    Add new projects

--- src/utils/data/projects.js
+++ src/utils/data/projects.js
@@ -1,5 +1,29 @@
 export const projects = [
   {
+    url: "https://calculadora-js-fcc.netlify.app",
+    img: "https://i.imgur.com/ZKakrwL.jpg",
+    name: "Calculadora",
+    description: "Calculadora realizada con React, Styled Components y MathJS."
+  },
+  {
+    url: "https://cajaderitmos.netlify.app",
+    img: "https://i.imgur.com/hAyqGBA.jpg",
+    name: "Caja de ritmos",
+    description: "Juega con los sonidos de la caja de ritmos presionando los botones de cada sonido. Hecho con React y Styled Components."
+  },
+  {
+    url: "https://relojpomodoro.netlify.app",
+    img: "https://i.imgur.com/UPr5rSU.jpg",
+    name: "Reloj Pomodoro",
+    description: "Temporizador pomodoro realizado con React y Styled Components. Controla la duración de la sesión y de descanso."
+  },
+  {
+    url: "https://frases-aleatorias.netlify.app/",
+    img: "https://i.imgur.com/T2NDoiN.jpg",
+    name: "Generador de frases aleatorias",
+    description: "Generador de frases de manera aleatoria con solo un click, con la posibilidad de realizar un tweet con la frase que más te guste. Hecho con React y Styled Components."
+  },
+  {
     url: "https://adopciondemascotas.netlify.app/",
     img: "https://i.imgur.com/w00muFc.jpg",
     name: "Adopta una mascota",
